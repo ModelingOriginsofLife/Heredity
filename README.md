ae880e2034b24ec8d68735d2abdb354c844e9480
Nicholas Guttenberg <ngutten@gmail.com>
2014-12-27 06:24:26 +0900

    Added command-line parameters to control the algorithm

--- README.md
+++ README.md
@@ -1,14 +1,38 @@
-Heredity
-========
+Heredity Metric
+===============
 
 Scripts for computing the heredity detection metric on a data set.
 
 Requirements: numpy, scikit-learn
 
-Usage: python ./computemetric.py datafile.txt
+Usage: python ./computemetric.py datafile.txt --<parameters>
 
 Output: The detected number of heritable states 
 
+Parameters:
+-----------
+
+--convergence FILENAME: Output convergence curve to a file
+
+--convstep STEPSIZE: Resolution of the convergence curve - how many
+samples to add each cycle
+			  
+--trials NUMTRIALS: When computing convergence curve, how many trials should 
+be used to estimate variance
+
+--eigvals FILENAME: Output eigenvalues to a file
+			    
+--eigvecs FILENAME: Output eigenvectors corresponding to heritable
+states to a file
+			  
+--regularization COPIES: Sets number of regularization duplicates to
+stabilize the algorithm in cases where there's no heredity
+
+--alpha ALPHA: Gap sensitivity parameter. Higher values take more data to 
+converge but are more robust.
+
+--zscore: Rescale features to zero mean and unit standard deviation
+			
 Data file format:
 -----------------
 
--- computemetric.py
+++ computemetric.py
@@ -1,7 +1,10 @@
 import numpy as np
 import sys
+import argparse
+import math
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import ShuffleSplit
 
 #########################################################################
 ###
@@ -19,24 +22,24 @@ from sklearn.preprocessing import StandardScaler
 ###
 #########################################################################
 
-# This is the sensitivity parameter alpha. If this is set to be very
-# small, the algorithm will be more sensitive but will be more likely
-# to blow up and give an anomalously large number of heritable states as its 
-# output.
-ALPHA = 0.1
+globalargs = {}
 
-def PCAmetric(population):
+def PCAmetric(population, finalpass = False):
 	count = 0
 	stdev = 0
 	fraction = 1
 	
-	subdata = population
-	# Uncomment this to normalize each feature by its standard deviation
-	# subdata = StandardScaler().fit_transform(population)
+	if globalargs.zscore:
+		subdata = StandardScaler().fit_transform(population)
+	else:
+		subdata = population
 
 	pca = PCA().fit(subdata)
 
 	latent = pca.explained_variance_ratio_
+	
+	if (globalargs.eigvals != None) and finalpass:
+		np.savetxt(globalargs.eigvals, latent, delimiter=' ')
 	
 	threshold = latent[latent.shape[0]-1]
 	
@@ -57,7 +60,16 @@ def PCAmetric(population):
 		
 		postmean /= norm
 		
-		threshold = (latent[0] - postmean)*ALPHA + postmean
+		# This is the sensitivity parameter alpha. If this is set to be very
+		# small, the algorithm will be more sensitive but will be more likely
+		# to blow up and give an anomalously large number of heritable states as its 
+		# output.
+		
+		threshold = (latent[0] - postmean)*globalargs.alpha + postmean
+	
+	if (globalargs.eigvecs != None) and (count>0) and finalpass:
+		np.savetxt(globalargs.eigvecs, pca.components_[0:count,:], delimiter=' ')
+		
 	return count
 
 # This function adds 10 copies of the first data row to the data set in
@@ -67,11 +79,60 @@ def PCAmetric(population):
 def doRegularization(data):
 	regulator = data[0]
 	newdata = np.copy(data)
-	for i in range(10):
+	for i in range(globalargs.regularization):
 		newdata = np.vstack( (newdata, regulator) )
 	
 	return newdata
 
-data = np.loadtxt(sys.argv[1])
+def myargs():
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description = 
+""" 
+Compute heredity metric and related quantities on a given set of data.
+\nUsage:
+\n
+\n\tpython computemetric.py dataset.txt --<parameters>
+\n
+""")
+	parser.add_argument('datafile', default = None, type=str,
+                        help='input data file')
+	parser.add_argument('--convergence', default = None, type = str, help = "Output convergence curve to a file (note, this will take significantly longer to compute!)")
+	parser.add_argument('--convstep', default = 1, type = int, help = "Resolution of the convergence curve - how many samples to add each cycle")
+	parser.add_argument('--trials', default = 1, type = int, help = "When computing convergence curve, how many trials should be used to estimate variance")
+	parser.add_argument('--eigvals', default = None, type = str, help = "Output eigenvalues to a file")
+	parser.add_argument('--eigvecs', default = None, type = str, help = "Output eigenvectors corresponding to heritable states to a file")
+	parser.add_argument('--regularization', default = 10, type = int, help = "Sets number of regularization duplicates to stabilize no-heredity case")
+	parser.add_argument('--alpha', default = 0.1, type = float, help = "Gap sensitivity parameter. Higher values take longer to converge but are more robust.")
+	parser.add_argument('--zscore', action = "store_true", help = "Rescale features to zero mean and unit standard deviation")
+  
+	args = parser.parse_args()
+        
+	return args
 
-print "{}".format(PCAmetric(doRegularization(data)))
+globalargs = myargs()
+
+if globalargs.datafile != None:
+	data = np.loadtxt(globalargs.datafile)
+
+	if globalargs.convergence != None:
+		for count in range(1,data.shape[0], globalargs.convstep):
+			detectedStates = 0.0
+			detectedStatesStdev = 0.0
+			shuffler = ShuffleSplit(data.shape[0], n_iter = globalargs.trials, test_size = count)
+			
+			for train_index, test_index in shuffler:
+				result = PCAmetric(doRegularization(data[test_index]), False)
+				detectedStates += result
+				detectedStatesStdev += result*result
+				
+			outfile = open(globalargs.convergence,"a")
+			if globalargs.trials == 1:
+				outfile.write("{} {}\n".format(count, detectedStates))
+			else:
+				detectedStates /= globalargs.trials
+				detectedStatesStdev /= globalargs.trials
+				detectedStatesStdev = math.sqrt(detectedStatesStdev - detectedStates*detectedStates)
+				outfile.write("{} {} {}\n".format(count, detectedStates, detectedStatesStdev))
+			outfile.close()
+			
+	print "{}".format(PCAmetric(doRegularization(data), True))
